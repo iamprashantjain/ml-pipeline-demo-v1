b986429663f37a261b21d52929d44d1d5301b090
Prashant Jain <iamprashantjain2601@gmail.com>
2026-08-25 23:48:16 +0530

    added dagshub token in start_docker run command

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -18,4 +18,8 @@ if [ "$(docker ps -aq -f name=my-flask-app)" ]; then
 fi
 
 # Run a new container
-docker run -d -p 80:5000 --name my-flask-app 739275446561.dkr.ecr.ap-south-1.amazonaws.com/prashant-ecr:latest
+docker run -d -p 80:5000 \
+  --name my-flask-app \
+  --restart always \
+  -e DAGSHUB_TOKEN="$DAGSHUB_TOKEN" \
+  739275446561.dkr.ecr.ap-south-1.amazonaws.com/prashant-ecr:latest
